80b67cac9249b76187fce9f6f1b1a9c23a1ac739
ferranramirez <ferranramireznavajon@gmail.com>
2020-06-01 09:37:24 +0200

    Add error display middleware

--- src/NG.NotGuiriAPI.Presentation.WebAPI/Startup.cs
+++ src/NG.NotGuiriAPI.Presentation.WebAPI/Startup.cs
@@ -39,6 +39,8 @@ namespace NG.NotGuiriAPI.Presentation.WebAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseErrorDisplayMiddleware();
+
             if (env.IsDevelopment()) { app.UseDeveloperExceptionPage(); }
 
             app.UseHttpsRedirection();
