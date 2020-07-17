d84531404041306055ce46e6bb904c83634f8aad
ferranramirez <ferranramireznavajon@gmail.com>
2020-07-17 10:58:02 +0200

    Update test

--- tst/NG.NotGuiriAPI.Test.UnitTest/TourServiceTests.cs
+++ tst/NG.NotGuiriAPI.Test.UnitTest/TourServiceTests.cs
@@ -1,25 +1,24 @@
 using Moq;
 using NG.DBManager.Infrastructure.Contracts.Models;
 using NG.DBManager.Infrastructure.Contracts.UnitsOfWork;
-using NG.DBManager.Test.Utilities;
 using NG.NotGuiriAPI.Business.Contract;
 using NG.NotGuiriAPI.Business.Impl;
+using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace NG.NotGuiriAPI.Test.UnitTest
 {
-    public class TourServiceTests : IClassFixture<DatabaseUtilities>
+    public class TourServiceTests/* : IClassFixture<DatabaseUtilities>*/
     {
-        private readonly DatabaseUtilities _databaseUtilities;
+        //private readonly DatabaseUtilities _databaseUtilities;
         private Mock<IAPIUnitOfWork> _uowMock;
         private ITourService _tourService;
 
-        public TourServiceTests(DatabaseUtilities databaseUtilities)
+        public TourServiceTests(/*DatabaseUtilities databaseUtilities*/)
         {
-            _databaseUtilities = databaseUtilities;
+            //_databaseUtilities = databaseUtilities;
 
             _uowMock = new Mock<IAPIUnitOfWork>();
             _tourService = new TourService(_uowMock.Object);
@@ -28,10 +27,8 @@ namespace NG.NotGuiriAPI.Test.UnitTest
         [Fact]
         public async Task GetFeaturedToursTest()
         {
-            //Arrange
-            IEnumerable<Tour> expected = _databaseUtilities.Tours
-                .Where(t => t.IsFeatured)
-                .ToList();
+            //Arrange            
+            IEnumerable<Tour> expected = new List<Tour>() { new Tour { Id = Guid.NewGuid() } };
 
             _uowMock.Setup(uow => uow.Tour.GetFeatured()).Returns(Task.FromResult(expected));
 
